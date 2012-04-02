17246601580c3dadea165e22812fb5bde1bc370c
Adam Vaughan <ajv@absolute-performance.com>
2012-04-02 07:57:37 -0600

    update use of ActiveSupport::Concern

--- lib/mobile_detection/detection.rb
+++ lib/mobile_detection/detection.rb
@@ -8,34 +8,32 @@ module MobileDetection
       helper_method :mobile_request?
     end
 
-    module InstanceMethods
-      def check_for_mobile_request
-        if mobile_request?
-          prepend_view_path Rails.root + 'app/views/mobile'
-          cookies[:mobile] = 'true'
-        end
+    def check_for_mobile_request
+      if mobile_request?
+        prepend_view_path Rails.root + 'app/views/mobile'
+        cookies[:mobile] = 'true'
+      end
 
-        if defined? ChromeHelpers::TemplateResolver
-          view_paths.each do |view_path|
-            if view_path.is_a? ChromeHelpers::TemplateResolver
-              view_path.mobile = (cookies[:mobile] == 'true')
-            end
+      if defined? ChromeHelpers::TemplateResolver
+        view_paths.each do |view_path|
+          if view_path.is_a? ChromeHelpers::TemplateResolver
+            view_path.mobile = (cookies[:mobile] == 'true')
           end
         end
       end
+    end
 
-      def mobile_browser?
-        request.user_agent =~ /Mobile|webOS|Android/ && !request.user_agent.include?('iPad')
-      end
+    def mobile_browser?
+      request.user_agent =~ /Mobile|webOS|Android/ && !request.user_agent.include?('iPad')
+    end
 
-      def mobile_request?
-        cookies[:mobile] = params[:mobile] if params[:mobile]
+    def mobile_request?
+      cookies[:mobile] = params[:mobile] if params[:mobile]
 
-        if cookies[:mobile]
-          cookies[:mobile] == 'true'
-        else
-          mobile_browser?
-        end
+      if cookies[:mobile]
+        cookies[:mobile] == 'true'
+      else
+        mobile_browser?
       end
     end
   end
